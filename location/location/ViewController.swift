2e68373fd8d4f82792e88802711a64e66fd91b2a
Andrew Finke <andrewfinke@me.com>
2020-01-24 04:12:15 -0600

    time to sleep

--- location/location/ViewController.swift
+++ location/location/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties -
+    
+    private let label = UILabel()
+    
     // MARK: - View Life Cycle -
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 88, weight: .medium)
+        view.addSubview(label)
+        
         animate()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        label.frame = view.bounds
     }
     
     // MARK: - Sign of Life -
@@ -26,6 +40,7 @@ class ViewController: UIViewController {
                                                 blue: .random(in: 0..<1),
                                                 alpha: 1.0)
         }, completion: { _ in
+            self.label.text = (UserDefaults.standard.array(forKey: "Locations") ?? []).count.description
             self.animate()
         })
     }
