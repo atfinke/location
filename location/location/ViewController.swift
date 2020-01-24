d1705196f6908810bb9a7fd2ed1255c3c28c5834
Andrew Finke <andrewfinke@me.com>
2020-01-24 04:09:18 -0600

    ds_store

---
+++ location/location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  location
+//
+//  Created by Andrew Finke on 1/24/20.
+//  Copyright © 2020 Andrew Finke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
