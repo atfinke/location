42caab4f09995ef1a74ef57f4aff131195acde0e
Andrew Finke <andrewfinke@me.com>
2020-01-24 14:46:29 -0600

    updates

--- location/location/Secrets.swift
+++ location/location/Secrets.swift
@@ -9,11 +9,4 @@
 import Foundation
 
 struct Secrets {
-    static let url: URL = {
-        let string = "https://magic-box-support.herokuapp.com/background/locations"
-        guard let url = URL(string: string) else {
-            fatalError()
-        }
-        return url
-    }()
 }
