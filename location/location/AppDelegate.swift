a72d917202ff634c8ffc27b6693437a5b62c6dc7
Andrew Finke <andrewfinke@me.com>
2020-01-24 04:09:35 -0600

    init

--- location/location/AppDelegate.swift
+++ location/location/AppDelegate.swift
@@ -7,31 +7,83 @@
 //
 
 import UIKit
+import CoreLocation
+import BackgroundTasks
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+    
+    
+    // MARK: - Properties -
+    
+    private let manager = CLLocationManager()
+    private var lastLocation: CLLocation?
+    private let backgroundTaskIdentifier = "com.andrewfinke.location.background"
+    
+    
+    // MARK: - UIApplicationDelegate -
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        manager.allowsBackgroundLocationUpdates = true
+        manager.pausesLocationUpdatesAutomatically = true
+        manager.startUpdatingLocation()
+        
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { task in
+            self.manager.stopUpdatingLocation()
+            self.manager.startUpdatingLocation()
+            
+            self.submitBackgroundTask()
+            task.setTaskCompleted(success: true)
+        }
+        
+        NotificationCenter.default.addObserver(forName: SceneDelegate.resignNotification, object: nil, queue: nil) { notification in
+            self.submitBackgroundTask()
+        }
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
+    // MARK: - CLLocationManagerDelegate -
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        let key = "Locations"
+        var existingLocations = UserDefaults.standard.array(forKey: key) as? [String] ?? []
+        let newLocations = locations.map { $0.description }
+        
+        existingLocations.append(contentsOf: newLocations)
+        UserDefaults.standard.set(existingLocations, forKey: key)
+        // Do something with the location.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let key = "Errors"
+        var errors = UserDefaults.standard.array(forKey: key) as? [String] ?? []
+        errors.append(error.localizedDescription)
+        UserDefaults.standard.set(errors, forKey: key)
+    }
+    
+    // MARK: - Background Tasks -
+    
+    private func submitBackgroundTask() {
+        DispatchQueue.global().async {
+            let request = BGAppRefreshTaskRequest(identifier: self.backgroundTaskIdentifier)
+            request.earliestBeginDate = Date(timeIntervalSinceNow: 60 * 10)
+            do {
+                try BGTaskScheduler.shared.submit(request)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    // MARK: - UISceneSession Lifecycle-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
 }
-
--- location/location/ViewController.swift
+++ location/location/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - View Life Cycle -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animate()
     }
-
+    
+    // MARK: - Sign of Life -
+    
+    func animate() {
+        UIView.animate(withDuration: 2.0, animations: {
+            self.view.backgroundColor = UIColor(displayP3Red: .random(in: 0..<1),
+                                                green: .random(in: 0..<1),
+                                                blue: .random(in: 0..<1),
+                                                alpha: 1.0)
+        }, completion: { _ in
+            self.animate()
+        })
+    }
 
 }
 
